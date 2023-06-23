351702065958a8aa000c5fb70d866883ff2b4d64
shreverr <verma2007s@gmail.com>
2023-06-23 13:11:29 +0530

    initial project setup

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="text-9xl dark:bg-slate-900 h-screen ">
+     yo app works
+    </div>
+  );
+}
+
+export default App;
