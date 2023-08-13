a03b1b3ff9b523ab75e2f848c8b2809e525a69d1
Shreshth Verma <verma2007s@gmail.com>
2023-08-13 15:08:12 +0530

    setup express server

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+require('dotenv').config()
+const app = express()
+const port = process.env.PORT
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Meetcode server is listening on port ${port}`)
+})
