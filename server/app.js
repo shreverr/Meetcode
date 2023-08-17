fb7bda7803e7b744774f7d343b1bbc56c0fcc430
Shreshth Verma <verma2007s@gmail.com>
2023-08-17 16:01:30 +0530

    minor readibility fix

--- server/app.js
+++ server/app.js
@@ -14,7 +14,7 @@ mongoose.connect(process.env.MONGODB_URI);
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error: "));
 db.once("open",  () => {
-  console.log("Connected successfully");
+  console.log("DB connected successfully");
 });
 
 app.listen(port, () => {
